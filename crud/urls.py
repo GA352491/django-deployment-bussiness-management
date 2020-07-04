69ddff8e6dc18ced3a44dad6caf352c285c25e39
anishganga <GA352491@gmail.com>
2020-07-04 17:47:21 +0530

    0

--- crud/urls.py
+++ crud/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from sales import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -37,6 +39,13 @@ urlpatterns = [
     path('deleteecustomer/<str:pk>', views.delete_customer, name='deletecustomer'),
     path('whatsapp/<str:pk>', views.whatsapp, name='whatsapp'),
     path('product/<str:pk>', views.pro_list, name='product'),
+
+
     path('accounts/', include('allauth.urls'), name='button'),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name='reset password.html'),
+         name='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name='password sent.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password.html'), name='password_reset_confirm'),
+    path('reset_password_complete/..', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
 ]
